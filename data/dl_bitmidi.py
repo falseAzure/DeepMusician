08209283c918bcf1dbf781d78047ead20cef5fa0
falseAzure <falseazure@proton.me>
2022-11-15 17:47:59 +0100

    Update dl_bitmidi.py
    Add some useful output

--- data/dl_bitmidi.py
+++ data/dl_bitmidi.py
@@ -10,27 +10,34 @@ def dl_bitmidi(m=0, n=125000):
     index from m to n."""
     url_stem = "https://bitmidi.com/uploads/"
     start = time.time()
+    last_midi = m - 1
+    count = 0
     for i in range(m, n):
-        print(f"Download Midi: {i}/{n}", end="\r")
+        print(
+            f"Processed Index: {i}/{n} - last MIDI file: {last_midi} - {i-last_midi} indices ago",
+            end="\r",
+        )
         file = str(i) + ".mid"
         url = url_stem + file
-        # if response is a 404, the file does not exist page
         while True:
             try:
                 response = requests.get(url, timeout=5)
                 break
             except requests.exceptions.Timeout:
                 continue
+        # If the response is 520, then the file doesn't exists
         if response.status_code == 520:
             continue
         try:
             open("data/bitmidi/" + file, "wb").write(response.content)
+            last_midi = i
+            count += 1
         except Exception:
             continue
     end = time.time()
     duration = str(datetime.timedelta(seconds=round(end - start)))
     print("")
-    print(f"Finished Downloading Midi Files from {m} to {n} in {duration}")
+    print(f"Finished Downloading {count} Midi Files from {m} to {n} in {duration}")
 
 
 def get_args():

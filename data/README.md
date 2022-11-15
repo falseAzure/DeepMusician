5987b00463e5d2d5509b7019de92cf5506267f1d
falseAzure <falseazure@proton.me>
2022-11-15 17:47:40 +0100

    update data download
    Update the download script and the corresponding README:
    - include a new source for the vgma
    - rename folders
    - add information in README

--- data/README.md
+++ data/README.md
@@ -1,6 +1,25 @@
 # Data
 
-To download the files into the corresponding folders please run
+The data consists of MIDI-files from several databases, that originate from different sources and
+cover a variety of genres:
+
+1. [Meastro](https://magenta.tensorflow.org/datasets/maestro) (maestro/) (1,291)
+2. [Classical Archives](https://www.classicalarchives.com>) (classical_archives/) (4,918)
+3. [Video Game
+   Music](https://archive.org/details/video-game-music-90000-midi-files) (vgm/) (92,861)
+4. [Video Game Music Archive](https://www.vgmusic.com/) (vgma/) (31,581)
+5. [Bitmidi](https://bitmidi.com/) (bitmidi/)
+6. [The Magic of MIDI V1](https://archive.org/details/themagicofmidiv1) (magic_of_midi/) (169,454)
+
+The number in parentheses indicates the location and the number of total
+titles. (1) and (2) cover pieces from *classical music*. (3) and (4) contain *video game* music. (5) and (6) comprise
+of all
+different sorts of music genres.
+
+MIDI-files capture the notes and timing of a piece of music with various
+additional information.
+
+To download the files into the corresponding folders please run:
 
 ```bash
 bash data/download.sh
--- data/download.sh
+++ data/download.sh
@@ -4,18 +4,22 @@
 wget "https://storage.googleapis.com/magentadata/datasets/maestro/v3.0.0/maestro-v3.0.0-midi.zip" -O "data/maestro-v3.0.0_midi.zip"
 unzip "data/maestro-v3.0.0_midi.zip" -d "data/"
 rm "data/maestro-v3.0.0_midi.zip"
+mv "data/maestro-v3.0.0" "data/maestro"
 
 # Classical Archives The Greats
 wget "https://archive.org/download/ClassicalArchivesTheGreatsMIDILibrary/Classical%20Archives%20-%20The%20Greats%20%28MIDI%20Library%29.rar" -O "data/ClassicalArchivesTheGreatsMIDILibrary.rar"
 unrar e "data/ClassicalArchivesTheGreatsMIDILibrary.rar" "data/"
 rm "data/ClassicalArchivesTheGreatsMIDILibrary.rar"
+mv "data/Classical Archives - The Greats (MIDI Library)" "data/classical_archives"
 
 # VG Music Archive
-wget "https://download1584.mediafire.com/kawkv7sqsvqg/i7q64yoj9j27xbu/2011-03-12-vgmusic.com.zip" -O "data/2011-03-12-vgmusic.com.zip"
-unzip "data/2011-03-12-vgmusic.com.zip" -d "data/vgma/"
-unzip data/vgma/\*.zip -d "data/vgma/"
-rm "data/2011-03-12-vgmusic.com.zip"
-rm data/vgma/*.zip
+wget "https://archive.org/download/31581VideogameMusicMIDIFileswReplayGain8mbgmsfx.sf2/31%2C581%20Videogame%20Music%20MIDI%20Files%20%28w%20Replay%20Gain%20-%208mbgmsfx.sf2%29.zip" -O "data/vgmusic.com.zip"
+unzip "data/vgmusic.com.zip" -d "data/"
+mv "data/31,581 Videogame Music MIDI Files (w Replay Gain - 8mbgmsfx.sf2)" "data/vgma"
+rm "data/vgmusic.com.zip"
+rm "data/S-YXG50-4MB.dll"
+rm "data/8mbgmsfx.sf2"
+find "data/vgma" -type d -empty -delete
 
 # Video Game Music
 wget "https://archive.org/compress/video-game-music-90000-midi-files/formats=ZIP&file=/video-game-music-90000-midi-files.zip" -O "data/video-game-music-90000-midi-files.zip"
@@ -28,3 +32,4 @@ rm data/vgm/*.zip
 wget "https://archive.org/download/themagicofmidiv1/The_Magic_of_MIDI.7z" -O "data/The_Magic_of_MIDI.7z"
 7z e "data/The_Magic_of_MIDI.7z" -o "data/"
 rm "data/The_Magic_of_MIDI.7z"
+mv "data/The_Magic_of_MIDI" "data/magic_of_midi"

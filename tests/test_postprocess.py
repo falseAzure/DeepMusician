f1b2652abb022e514c5b1cd4379ece7b611964cb
falseAzure <falseazure@proton.me>
2022-11-25 11:42:25 +0100

    Init tests

---
+++ tests/test_postprocess.py
@@ -0,0 +1,3 @@
+# assert all have 88 notes
+# assert length of pianorolls
+# assert only 0 and 1 in pianorolls
--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,2 +1,7 @@
+# assert all have 88 notes
+# assert length of pianorolls
+# assert only 0 and 1 in pianorolls
+
+
 def test_test():
     assert 1 == 1

25422440ba8e659fefe6093d8a8c5496a7109bdc
falseAzure <falseazure@proton.me>
2022-11-26 19:02:45 +0100

    Update utils_music21.py
    - Refactor
    - Add functionality for drum tracks
    - minor changes

--- deepmusician/utils_music21.py
+++ deepmusician/utils_music21.py
@@ -1,11 +1,20 @@
+"""
+Provides utils functions for the preprocessing of the midi files with music21.
+"""
+
 import os
+import warnings
 
 import music21
 import numpy as np
 import pandas as pd
 
 
-def get_numpy_notes_from_instrument(instrument):
+def get_notes_np_from_instrument(instrument):
+    """Transform the notes given by an instrument track into a 2D (n_notes, 4)
+    numpy array, that holds pitch, offset, velocity and duration of the each
+    individual note. Chords are separated into the individual notes.
+    """
     notes = instrument.recurse().notes
     pitch = []
     offset = []
@@ -20,29 +29,36 @@ def get_numpy_notes_from_instrument(instrument):
                 offset.append(note.offset)
                 velocity.append(note.volume.velocity)
                 duration.append(note.duration.quarterLength)
+        elif isinstance(note, music21.note.Unpitched) or isinstance(
+            note, music21.percussion.PercussionChord
+        ):
+            pitch.append(0)
+            offset.append(note.offset)
+            velocity.append(note.volume.velocity)
+            duration.append(note.duration.quarterLength)
         else:
             pitch.append(note.pitch.midi)
             offset.append(note.offset)
             velocity.append(note.volume.velocity)
             duration.append(note.duration.quarterLength)
 
-    np_notes = np.array([offset, pitch, velocity, duration], dtype=float).T
-    return np_notes
+    notes_np = np.array([offset, pitch, velocity, duration], dtype=float).T
+    return notes_np
 
 
-def get_numpy_notes_from_m21(m21_midi):
-    instruments = music21.instrument.partitionByInstrument(m21_midi)
-    np_notes_all = np.empty((0, 5), dtype=float)
+def get_notes_np_from_m21(midi21):
+    instruments = music21.instrument.partitionByInstrument(midi21)
+    notes_all_np = np.empty((0, 5), dtype=float)
     for i, instrument in enumerate(instruments):
-        np_notes = get_numpy_notes_from_instrument(instrument)
-        np_notes = np.append(np_notes, np.full((len(np_notes), 1), i), axis=-1)
-        np_notes_all = np.append(np_notes_all, np_notes, axis=0)
-    return np_notes_all
+        notes_np = get_notes_np_from_instrument(instrument)
+        notes_np = np.append(notes_np, np.full((len(notes_np), 1), i), axis=-1)
+        notes_all_np = np.append(notes_all_np, notes_np, axis=0)
+    return notes_all_np, instruments
 
 
-def get_numpy_notes_from_file(fname):
-    midi = music21.converter.parse(fname)
-    return get_numpy_notes_from_m21(midi)
+def get_notes_np_from_file(fname):
+    midi21 = music21.converter.parse(fname)
+    return get_notes_np_from_m21(midi21)
 
 
 def extract_info_instruments(instruments):
@@ -55,26 +71,43 @@ def extract_info_instruments(instruments):
     }
 
 
-def get_numpy_notes_from_files(midi_files=list):
+# def extract_info_instruments(instruments):
+#     info_instruments = {}
+#     for i, ins in enumerate(instruments):
+#         with warnings.catch_warnings():
+#             warnings.simplefilter("ignore")
+#             instrument_name = ins.getInstrument().partName
+#             instrument_type = ins.partName
+#         info_instruments[i] = {
+#             "instrument_name": instrument_name,
+#             "instrument_type": instrument_type,
+#         }
+#     return info_instruments
+
+
+def get_notes_np_from_files(midi_files=list):
     meta_dict = {}
-    np_notes_all = np.empty((0, 6), dtype=float)
+    notes_all_np = np.empty((0, 6), dtype=float)
     for i, fname in enumerate(midi_files):
         name = os.path.basename(fname)
         print(f"Processing {i+1}/{len(midi_files)}: {name:100}", end="\r", flush=True)
-        midi = music21.converter.parse(fname)
+        # music21 issues warning if instrument is unknown
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            midi21 = music21.converter.parse(fname)
+        notes_np, instruments = get_notes_np_from_m21(midi21)
+        notes_np = np.append(notes_np, np.full((len(notes_np), 1), i), axis=-1)
+        notes_all_np = np.append(notes_all_np, notes_np, axis=0)
         meta_dict[i] = {
             "filename": fname,
-            # "instruments": dict(extract_info_instruments(midi.parts)),
+            "instruments": dict(extract_info_instruments(instruments)),
         }
-        np_notes = get_numpy_notes_from_m21(midi)
-        np_notes = np.append(np_notes, np.full((len(np_notes), 1), i), axis=-1)
-        np_notes_all = np.append(np_notes_all, np_notes, axis=0)
-    return np_notes_all, meta_dict
+    return notes_all_np, meta_dict
 
 
-def numpy_notes_to_pd(np_notes):
-    df_notes = pd.DataFrame(
-        np_notes,
+def notes_np_to_df(notes_np):
+    notes_df = pd.DataFrame(
+        notes_np,
         columns=[
             "offset",
             "pitch",
@@ -84,65 +117,61 @@ def numpy_notes_to_pd(np_notes):
             "midi_id",
         ],
     )
-    df_notes["track_id"] = df_notes.groupby(["midi_id", "instrument_id"]).ngroup()
-    return df_notes
+    notes_df["track_id"] = notes_df.groupby(["midi_id", "instrument_id"]).ngroup()
+    return notes_df
 
 
 def meta_dict_to_df(meta_dict):
     midi_id = []
     midi_name = []
-    # ins_program = []
-    # ins_name = []
-    # for idx in meta_dict.keys():
-    #     for ins in meta_dict[idx]["instruments"].keys():
-    #         midi_id.append(idx)
-    #         midi_name.append(meta_dict[idx]["filename"])
-    #         ins_program.append(ins)
-    #         ins_name.append(meta_dict[idx]["instruments"][ins]["instrument_name"])
-            
+    ins_program = []
+    ins_name = []
     for idx in meta_dict.keys():
-        midi_id.append(idx)
-        midi_name.append(meta_dict[idx]["filename"])
+        for ins in meta_dict[idx]["instruments"].keys():
+            midi_id.append(idx)
+            midi_name.append(meta_dict[idx]["filename"])
+            ins_program.append(ins)
+            ins_name.append(meta_dict[idx]["instruments"][ins]["instrument_name"])
 
-    df_meta = pd.DataFrame(
+    meta_df = pd.DataFrame(
         {
             "midi_id": midi_id,
             "midi_name": midi_name,
-            # "ins_program": ins_program,
-            # "ins_name": ins_name,
+            "ins_program": ins_program,
+            "ins_name": ins_name,
         }
     )
-    return df_meta
+    return meta_df
 
 
-def df_track_to_pianoroll(df_notes, track, division=1 / 16):
+def df_track_to_pianoroll(notes_df, track, division=1 / 16):
     offset = np.round(
         np.array(
-            df_notes[df_notes.track_id == track].offset / (division * 4), dtype=int
+            notes_df[notes_df.track_id == track].offset / (division * 4), dtype=int
         )
     )
-    pitch = np.array(df_notes[df_notes.track_id == track].pitch - 21, dtype=int)
+    pitch = np.array(notes_df[notes_df.track_id == track].pitch - 21, dtype=int)
     assert len(offset) == len(pitch)
 
     pianoroll = np.zeros((int(offset.max()) + 1, 88), dtype=int)
-    for i, o_ in enumerate(offset):
-        # only pinao notes
+    for i, off in enumerate(offset):
+        # only piano notes
         if pitch[i] < 88 and pitch[i] >= 0:
-            pianoroll[o_, pitch[i]] = 1
+            pianoroll[off, pitch[i]] = 1
 
     return pianoroll
 
 
-def df_to_pianorolls(df_notes, division=1 / 16):
+def df_to_pianorolls(notes_df, division=1 / 16):
     pianorolls = []
-    for track in df_notes.track_id.unique():
-        pianorolls.append(df_track_to_pianoroll(df_notes, track, division))
+    for track in notes_df.track_id.unique():
+        pianorolls.append(df_track_to_pianoroll(notes_df, track, division))
     return pianorolls
 
 
-def process_midi_files(midi_files, division=1 / 16):
-    np_notes_all, meta_dict = get_numpy_notes_from_files(midi_files)
-    df_notes = numpy_notes_to_pd(np_notes_all)
-    df_meta = meta_dict_to_df(meta_dict)
-    pianorolls = df_to_pianorolls(df_notes, division)
-    return pianorolls, df_notes, df_meta
+def process_midi_files(midi_files=list, division=1 / 16):
+    notes_all_all, meta_dict = get_notes_np_from_files(midi_files)
+    notes_df = notes_np_to_df(notes_all_all)
+    meta_df = meta_dict_to_df(meta_dict)
+    pianorolls = df_to_pianorolls(notes_df, division)
+    return pianorolls, notes_df, meta_df

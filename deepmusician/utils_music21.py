eec5928a0c18694a7d98439d59de77055a3e7d70
falseAzure <falseazure@proton.me>
2022-12-13 23:39:58 +0100

    Update utils_music21.py
    Add description
    Minor refactoring

--- deepmusician/utils_music21.py
+++ deepmusician/utils_music21.py
@@ -1,5 +1,5 @@
 """
-Provides utils functions for the preprocessing of the midi files with music21.
+Provides utils functions to preprocess midi files with music21.
 """
 
 import os
@@ -10,13 +10,20 @@ import music21
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import torch
 
 
 def get_notes_np_from_instrument(instrument):
-    """Transform the notes given by an instrument track into a 2D (n_notes, 4)
-    numpy array, that holds pitch, offset, velocity and duration of the each
-    individual note. Chords are separated into the individual notes.
+    """
+    Transform the notes given by an music21.instrument object into a 2D numpy
+    array (n_notes, 4), that holds pitch, offset, velocity and duration of the
+    each individual note. Chords are separated into the individual notes with
+    the same offset.
+
+    Args:
+        instrument: instrument of a music21 midi object
+
+    Returns:
+        numpy.array: contains the notes of the instrument track (n_notes, 4)
     """
     notes = instrument.recurse().notes
     pitch = []
@@ -50,6 +57,21 @@ def get_notes_np_from_instrument(instrument):
 
 
 def get_notes_np_from_m21(midi21):
+    """
+    Transform the notes given by an music21.midi object into a 2D numpy array
+    (n_notes, 5). It does so by iterating over all the individual instrument
+    tracks of a midi file and calling get_notes_np_from_instrument. The first 4
+    parameters of each note thus represent the same as in
+    get_notes_np_from_instrument (pitch, offset, velocity, duration), while the
+    last parameter is the instrument number.
+
+    Args:
+        midi21: music21 midi object
+
+    Returns:
+        numpy.array: contains the notes of the midi file (n_notes, 5)
+        instruments: list of instrument tracks of the midi file
+    """
     instruments = music21.instrument.partitionByInstrument(midi21)
     notes_all_np = np.empty((0, 5), dtype=float)
     valid_instruments = [ins for ins in instruments if len(ins.notes) > 0]
@@ -61,11 +83,13 @@ def get_notes_np_from_m21(midi21):
 
 
 def get_notes_np_from_file(midi_file):
+    """Wrapper function for get_notes_np_from_m21 to directly read a midi file"""
     midi21 = music21.converter.parse(midi_file)
     return get_notes_np_from_m21(midi21)
 
 
 def extract_info_instruments(instruments):
+    """Extracts the instrument name and type from a list of instruments"""
     return {
         i: {
             "instrument_name": ins.getInstrument().partName,
@@ -75,21 +99,22 @@ def extract_info_instruments(instruments):
     }
 
 
-# def extract_info_instruments(instruments):
-#     info_instruments = {}
-#     for i, ins in enumerate(instruments):
-#         with warnings.catch_warnings():
-#             warnings.simplefilter("ignore")
-#             instrument_name = ins.getInstrument().partName
-#             instrument_type = ins.partName
-#         info_instruments[i] = {
-#             "instrument_name": instrument_name,
-#             "instrument_type": instrument_type,
-#         }
-#     return info_instruments
+def get_notes_np_from_files(midi_files: list):
+    """
+    Transforms the notes of a list of midi files into a 2D numpy array
+    (n_notes, 6). It's a wrapper function for get_notes_np_from_m21 to directly
+    read a list of midi files. It iterates over all the midi files and calls
+    get_notes_np_from_m21 for each of them. The additional parameter of each
+    note represents the file.
 
+    Args:
+        midi_files (list): List of midi files
 
-def get_notes_np_from_files(midi_files=list):
+    Returns:
+        numpy.array: contains the notes of the midi files (n_notes, 6)
+        dictionary: contains the filenames and the instrument information of
+        the midi files.
+    """
     meta_dict = {}
     notes_all_np = np.empty((0, 6), dtype=float)
     for i, fname in enumerate(midi_files):
@@ -110,6 +135,17 @@ def get_notes_np_from_files(midi_files=list):
 
 
 def notes_np_to_df(notes_np):
+    """
+    Transforms a 2D numpy array of notes into a pandas dataframe.
+
+    Args:
+        notes_np (numpy.array): 2D numpy array of notes (n_notes, 6). Usually
+        obtained by get_notes_np_from_files
+
+    Returns:
+        pandas.DataFrame: Dataframe containing the notes of the midi files and
+        its parameters (offset, pitch, velocity, duration, instrument_id, file_id)
+    """
     notes_df = pd.DataFrame(
         notes_np,
         columns=[
@@ -118,28 +154,29 @@ def notes_np_to_df(notes_np):
             "velocity",
             "duration",
             "instrument_id",
-            "midi_id",
+            "file_id",
         ],
     )
-    notes_df["track_id"] = notes_df.groupby(["midi_id", "instrument_id"]).ngroup()
+    notes_df["track_id"] = notes_df.groupby(["file_id", "instrument_id"]).ngroup()
     return notes_df
 
 
 def meta_dict_to_df(meta_dict):
-    midi_id = []
+    """Transforms a dictionary of meta information into a pandas dataframe."""
+    file_id = []
     midi_name = []
     ins_program = []
     ins_name = []
     for idx in meta_dict.keys():
         for ins in meta_dict[idx]["instruments"].keys():
-            midi_id.append(idx)
+            file_id.append(idx)
             midi_name.append(meta_dict[idx]["filename"])
             ins_program.append(ins)
             ins_name.append(meta_dict[idx]["instruments"][ins]["instrument_name"])
 
     meta_df = pd.DataFrame(
         {
-            "midi_id": midi_id,
+            "file_id": file_id,
             "midi_name": midi_name,
             "ins_program": ins_program,
             "ins_name": ins_name,
@@ -149,6 +186,21 @@ def meta_dict_to_df(meta_dict):
 
 
 def df_track_to_pianoroll(notes_df, division=1 / 16):
+    """
+    Transforms a track (represented by a DataFrame of notes) into a 2D numpy
+    (track length, 88), that represents a pianoroll. The first dimension
+    captures the time and is defined by the offset of the notes. The second
+    dimension captures the pitch of the notes: 88 is the number of piano notes
+    (pitch: 21-108).
+
+    Args:
+        notes_df (DataFrame): DataFrame containing the notes of the midi files.
+        division (float, optional): The granularity/division to subdivide each
+        bar. E.g. the smallest possible distance between two notes. Defaults to 1/16.
+
+    Returns:
+        numpy.array: 2D numpy array (track length, 88) that represents a pianoroll.
+    """
     offset = np.round(np.array(notes_df.offset / (division * 4), dtype=int))
     pitch = np.array(notes_df.pitch - 21, dtype=int)
     assert len(offset) == len(pitch)
@@ -165,6 +217,11 @@ def df_track_to_pianoroll(notes_df, division=1 / 16):
 
 
 def df_to_pianorolls(notes_df, division=1 / 16):
+    """
+    Transforms a DataFrame of notes into a list of pianorolls. Its a wrapper
+    of df_track_to_pianoroll that iterates over all the tracks of the
+    DataFrame. Division is the granularity/division to subdivide each bar.
+    """
     pianorolls = []
     for track in notes_df.track_id.unique():
         pianorolls.append(
@@ -174,6 +231,11 @@ def df_to_pianorolls(notes_df, division=1 / 16):
 
 
 def process_midi_files(midi_files=list, division=1 / 16):
+    """
+    Wrapper of the functions to process a list of midi files. It returns a list
+    of pianorolls, a DataFrame of notes and a DataFrame of meta information.
+    Division is the granularity/division to subdivide each bar.
+    """
     notes_all_all, meta_dict = get_notes_np_from_files(midi_files)
     notes_df = notes_np_to_df(notes_all_all)
     meta_df = meta_dict_to_df(meta_dict)
@@ -182,25 +244,15 @@ def process_midi_files(midi_files=list, division=1 / 16):
 
 
 def get_density(pianoroll):
-    return sum(sum(pianoroll)) / len(pianoroll)
+    return pianoroll.sum() / len(pianoroll)
 
 
-def get_train_val_test(pianorolls, train=0.8, val=0.1, test=0.1):
-    assert train + val + test == 1
-    n = len(pianorolls)
-    train_idx = int(n * train)
-    val_idx = int(n * (train + val))
-    return (
-        torch.tensor(np.concatenate(pianorolls[:train_idx], axis=0)),
-        torch.tensor(np.concatenate(pianorolls[train_idx:val_idx], axis=0)),
-        torch.tensor(np.concatenate(pianorolls[val_idx:], axis=0)),
-        range(train_idx),
-        range(train_idx, val_idx),
-        range(val_idx, n),
-    )
-
-
-def plot_pianoroll(pianoroll, length=128):
+def plot_pianoroll(pianoroll, length=128, division=1 / 16):
+    """
+    Plot a pianoroll as a heatmap, that depicts the notes played at each
+    time step. Division is the granularity/division to subdivide each bar.
+    """
+    assert sum(sum(pianoroll)) > 0, "pianoroll is empty"
     min_note = min(np.where(pianoroll[:length] == 1)[1])
     max_note = max(np.where(pianoroll[:length] == 1)[1])
     first_note = min(np.where(pianoroll == 1)[0])
@@ -222,12 +274,16 @@ def plot_pianoroll(pianoroll, length=128):
         yticklabels=x_notes,
     )
     ax.invert_yaxis()
-    plt.xlabel("1/16 Notes")
+    plt.xlabel(str(division) + "Notes")
     plt.ylabel("Note")
     plt.show()
 
 
 def pianoroll_to_note(pianoroll, division=1 / 16):
+    """
+    Backtransform a pianoroll into a list of music21 notes. Division is the
+    granularity/division to subdivide each bar.
+    """
     music21_notes = []
     for i in range(len(pianoroll)):
         n_notes = sum(
@@ -267,6 +323,9 @@ def pianoroll_to_note(pianoroll, division=1 / 16):
 
 
 def notes_to_midi(music21_notes, save=None):
+    """
+    Backtransforms a list of music21 notes into a midi file.
+    """
     midi_stream = music21.stream.Stream(
         music21_notes, timeSignature=music21.meter.TimeSignature("4/4")
     )
@@ -277,11 +336,13 @@ def notes_to_midi(music21_notes, save=None):
 
 
 def pianoroll_to_midi(pianoroll, division=1 / 16, save=None):
+    """Wrapper of pianoroll_to_note and notes_to_midi."""
     music21_notes = pianoroll_to_note(pianoroll, division)
     return notes_to_midi(music21_notes, save)
 
 
 def pianoroll_to_df(pianoroll):
+    """Backtransform a pianoroll into a DataFrame of notes."""
     division = 1 / 16
     notes_df = pd.DataFrame(pianoroll, columns=["note" + str(i) for i in range(88)])
     notes_df["id"] = notes_df.index
@@ -293,5 +354,5 @@ def pianoroll_to_df(pianoroll):
     notes_df["duration"] = 1
     notes_df.sort_index(inplace=True)
     notes_df.reset_index(drop=True, inplace=True)
-    notes_df['n_notes'] = notes_df.groupby('offset').transform('count')['note']
+    notes_df["n_notes"] = notes_df.groupby("offset").transform("count")["note"]
     return notes_df

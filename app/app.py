2f241af73b5a132935cd401930b9ab99c7df68ee
falseAzure <falseazure@proton.me>
2023-01-14 13:23:42 +0100

    Update app.py
    Refactor and comment

--- app/app.py
+++ app/app.py
@@ -17,30 +17,35 @@ model.load_from_checkpoint(model_file)
 def home():
     return render_template("index.html")
 
-
-@app.route("/predict", methods=["POST"])
-def predict():
+# route to the generate page
+@app.route("/generate", methods=["POST"])
+def generate():
     """
     For rendering results on HTML GUI
     """
 
+    # get the sequence length from the form
     int_features = [int(x) for x in request.form.values()]
     seq_len = int_features[0]
+    # generate the sequence
     seq = model.generate_sequence(seq_len=seq_len)
 
     # fig = plot_pianoroll(seq, division=1 / 4, return_plot=True)
     # img = fig_to_base64(fig)
 
+    # convert the sequence to midi
     mf = pianoroll_to_midi(seq)
+    # convert the midi to base64 so that it can be rendered in the browser
     midi = io.BytesIO()
     mf.writeFile(midi)
     midi.seek(0)
     midi = base64.b64encode(midi.getvalue()).decode("utf-8")
 
+    # return the generated sequence as a midi file encoded in base64
     return render_template(
         "index.html",
         midi=midi,
-        # prediction_text=f"Your generated sequence of length {seq_len}:",
+        # generation_text=f"Your generated sequence of length {seq_len}:",
         # img=img,
     )
 
